12628d9f501b60fe96cab74500b4061d306484f6
Brian Boyle <email@brianboyle.ie>
2020-06-21 23:14:00 +0100

    with api to view stats

--- little_elixir_otp_guidebook/metex/lib/worker.ex
+++ little_elixir_otp_guidebook/metex/lib/worker.ex
@@ -10,6 +10,10 @@ defmodule Metex.Worker do
         GenServer.call(pid, {:location, location})
     end
 
+    def get_stats(pid) do
+        GenServer.call(pid, :get_stats)
+    end
+
     # Server Callbacks
     def init(:ok) do
         {:ok, %{}}
@@ -18,12 +22,16 @@ defmodule Metex.Worker do
     def handle_call({:location, location}, _from, stats) do
         case temperature_of(location) do
             {:ok, temp} ->
-                # new_stats = update_stats(stats, location)
-                {:reply, "#{temp}C", stats}
+                new_stats = update_stats(stats, location)
+                {:reply, "#{temp}C", new_stats}
             _ ->
                 {:reply, :error, stats}
         end
         # {:reply, location, stats}
+    end
+
+    def handle_call(:get_stats, _from, stats) do
+        {:reply, stats, stats}
     end
 
     # Helper Functions - (Functional Core)
