8e7ca4cc602cf8a5a96e2a6831562e25515a54b4
Brian Boyle <email@brianboyle.ie>
2020-06-21 10:32:56 +0100

    removed pry calls

--- little_elixir_otp_guidebook/metex/lib/coordinator.ex
+++ little_elixir_otp_guidebook/metex/lib/coordinator.ex
@@ -1,17 +1,14 @@
 defmodule Metex.Coordinator do
-    require IEx
 
     def loop(results \\ [], results_expected) do
         receive do 
             {:ok, result} ->
                 new_results = [result | results]
                 if results_expected == Enum.count(new_results) do
-                    IEx.pry
                     send self(), :exit
                 end
                 loop(new_results, results_expected)
             :exit ->
-                IEx.pry
                 IO.puts(results |> Enum.sort |> Enum.join(", "))
             _ ->
                 loop(results, results_expected)
