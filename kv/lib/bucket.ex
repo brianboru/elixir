19541144c81a533d2e87e3e1f8690175d0b3eed6
Brian Boyle <email@brianboyle.ie>
2021-08-20 09:54:14 +0100

    elixir docs example

---
+++ kv/lib/bucket.ex
@@ -0,0 +1,33 @@
+defmodule KV.Bucket do
+  use Agent
+
+  @doc """
+  Starts a new bucket
+  """
+  @spec start_link(any) :: {:error, any} | {:ok, pid}
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  @doc """
+  Gets a value from the `bucket` by `key`
+  """
+  @spec get(atom | pid | {atom, any} | {:via, atom, any}, any) :: any
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
+
+  @doc """
+  Puts the `value` for the given `key` in to the `bucket`
+  """
+  @spec put(atom | pid | {atom, any} | {:via, atom, any}, any, any) :: :ok
+  def put(bucket, key, value) do
+    # &Map.put(&1....)
+    # is short hand for
+    # fn state -> Map.put(state, key, value)
+    # Agent.update/3 (last arg has default value) requires a function as param 2
+    # the function has a single parameter of current state and the new state will
+    # be that returned by the function
+    Agent.update(bucket, &Map.put(&1, key, value))
+  end
+end
